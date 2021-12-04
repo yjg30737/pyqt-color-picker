c9922ae3f8b91303edd29d7e4e34596d56191788
Jung Gyu Yoon <yjg30737@gmail.com>
2021-12-04 10:49:40 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt5-color-dialog',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt5 Color Dialog',
+    url='https://github.com/yjg30737/pyqt5-color-dialog.git',
+    install_requires=[
+        'PyQt5>=5.8'
+    ]
+)
